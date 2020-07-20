0834d61217e1dc053016e1fa18dac45a151f4d02
LucaRiccio <lucariccio888@gmail.com>
2020-07-20 15:29:21 +0200

    Array + Generazione random 16 num.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-alert("prova");
+// Il computer deve generare 16 numeri casuali tra 1 e 100.
+// I numeri non possono essere duplicati
+// In seguito deve chiedere all'utente (100 - 16) volte di inserire un numero alla volta,
+// sempre compreso tra 1 e 100.
+// L’utente non può inserire più volte lo stesso numero.
+// Se il numero è presente nella lista dei numeri generati, la partita termina,
+// altrimenti si continua chiedendo all'utente un altro numero.
+// La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge
+// il numero massimo possibile di numeri consentiti.
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero
+//  di volte che l’utente ha inserito un numero consentito.
+
+
+var numeriPc = [];
+var numeriUtente = [];
+
+for (var i = 0; i < 16; i++) {
+  numeriPc[i] = Math.floor((Math.random() * 100) + 1);
+}
+
+console.log(numeriPc);
